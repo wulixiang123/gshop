9d7db616521a9c469160c4be20d641297d5db8d8
liuyuanbo <1131107655@qq.com>
2023-07-26 13:45:30 +0800

    skuform-静态搭建

--- src/views/product/spu/components/SkuForm/index.vue
+++ src/views/product/spu/components/SkuForm/index.vue
@@ -1,9 +1,81 @@
 <template>
-    <div>
-      <div>SkuForm - 新增SKU</div>
-      <el-button @click="emits('update:modelValue', STATUS.SPULIST)">取消</el-button>
-    </div>
-  </template>
+  <el-form label-width="100px">
+    <el-form-item label="SPU名称">
+      <div>xxx</div>
+    </el-form-item>
+
+    <el-form-item label="SKU名称">
+      <el-input placeholder="SUK名称"></el-input>
+    </el-form-item>
+
+    <el-form-item label="价格(元)">
+      <el-input placeholder="价格(元)"></el-input>
+    </el-form-item>
+
+    <el-form-item label="重量(千克)">
+      <el-input placeholder="重量(千克)"></el-input>
+    </el-form-item>
+
+    <el-form-item label="规格描述">
+      <el-input type="textarea" placeholder="规格描述"></el-input>
+    </el-form-item>
+
+    <el-form-item label="平台属性">
+      <el-form inline>
+        <el-form-item label="手机系统">
+          <el-select>
+            <el-option :value="1001" label="北京"></el-option>
+            <el-option :value="1002" label="上海"></el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="电池容量">
+          <el-select>
+            <el-option :value="1001" label="北京"></el-option>
+            <el-option :value="1002" label="上海"></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </el-form-item>
+
+    <el-form-item label="销售属性">
+      <el-form inline>
+    <el-form-item label="颜色">
+      <el-select>
+        <el-option :value="1001" label="北京"></el-option>
+        <el-option :value="1002" label="上海"></el-option>
+      </el-select>
+    </el-form-item>
+
+    <el-form-item label="版本">
+      <el-select>
+        <el-option :value="1001" label="北京"></el-option>
+        <el-option :value="1002" label="上海"></el-option>
+      </el-select>
+    </el-form-item>
+  </el-form>
+</el-form-item>
+
+<el-form-item label="图片列表">
+  <el-table border>
+    <el-table-column type="selection" width="80" align="center"></el-table-column>
+    <el-table-column label="图片"></el-table-column>
+    <el-table-column label="名称"></el-table-column>
+    <el-table-column label="操作" width="100">
+      <template #default="{ row, $index }">
+        <el-button type="primary" size="small">设为默认</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</el-form-item>
+
+
+<el-form-item>
+  <el-button type="primary">保存</el-button>
+  <el-button>取消</el-button>
+</el-form-item>
+  </el-form>    
+</template>
   
   <script setup lang="ts">
   import { STATUS } from '../../index.vue'

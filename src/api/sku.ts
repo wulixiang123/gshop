025ec09a715f97b51474dd0a6a47fe0c02df8226
liuyuanbo <1131107655@qq.com>
2023-07-28 11:12:27 +0800

    sku初始化数据展示

--- src/api/sku.ts
+++ src/api/sku.ts
@@ -1,6 +1,41 @@
 import request from '@/utils/request'
 import type { SpuImageModel } from './spu'
 
+
+export interface SkuImgData {
+    id?: number,
+    skuId?: number,
+    imgName: string,
+    imgUrl: string,
+    spuImgId: number,
+    isDefault?: string
+  }
+
+
+  export interface SkuAttrValueData {
+    id?: number,
+    attrId: number | string,
+    valueId: number | string,
+    skuId?: number,
+    attrName?: string,
+    valueName?: string
+  }
+
+
+
+
+  export interface SkuSaleAttrValueData {
+    id?: number,
+    skuId?: number,
+    spuId?: number,
+    saleAttrValueId: number | string,
+    saleAttrId: number | string,
+    saleAttrName?: string,
+    saleAttrValueName?: string
+  }
+
+
+
 export interface SkuAttrValueModel{
     attrId:string
     valueId:string
@@ -27,6 +62,37 @@ export interface SkuModel {
     skuAttrValueList: SkuAttrValueModel[] // 收集到的平台属性
     skuSaleAttrValueList: skuSaleAttrValueModel[] // 收集到的销售属性
   }
+
+
+export type SkuPageListData = {
+    records: skuInfoDate[]
+    total: number
+    size: number
+    current: number
+    searchCount: boolean
+    pages: number
+}
+
+
+
+export type skuInfoDate={
+    id?: number | string
+    spuId?: number
+    price: number
+    skuName: string
+    skuDesc: string
+    weight: string
+    tmId?: number
+    category3Id?: number
+    skuDefaultImg: string
+    isSale?: number
+    skuImageList: SkuImgData[]
+    createTime?: string
+    skuAttrValueList: SkuAttrValueData[]
+    skuSaleAttrValueList: SkuSaleAttrValueData[]
+}
+
+
   
 
 export default {
@@ -37,5 +103,9 @@ export default {
     // 保存sku
     reqSave(data:SkuModel){
         return request.post<any,null>(`/admin/product/saveSkuInfo`, data)
+    },
+    // 获取sku分页列表数据
+    reqSkuList(page:number,limit:number){
+        return request.get<any,SkuPageListData>(`admin/product/list/${page}/${limit}`)
     }
 }
--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,9 +1,64 @@
 <template>
-  <div>sku管理</div>
+  <div>
+    <el-table border :data="skuList">
+      <el-table-column label="序号" width="80px" align="center" type="index"></el-table-column>
+      <el-table-column label="名称" prop="skuName"></el-table-column>
+      <el-table-column label="描述" prop="skuDesc"></el-table-column>
+      <el-table-column label="默认图片">
+        <template #default="{row,$index}">
+          <img :src="row.skuDefaultImg" style="width: 100px;height: 60px;">
+        </template>
+      </el-table-column>
+      <el-table-column label="重量(KG)" prop="weight"></el-table-column>
+      <el-table-column label="价格(元)" prop="price"></el-table-column>
+      <el-table-column label="操作" align="center" width="250">
+        <template #default="{row,$index}">
+          <el-button type="success" size="small" icon="ele-Top"></el-button>
+          <el-button type="primary" size="small" icon="ele-Edit"></el-button>
+          <el-button type="info" size="small" icon="ele-InfoFilled"></el-button>
+          <el-button type="danger" size="small" icon="ele-Delete"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+
+    <el-pagination v-model:currentPage="page" v-model:page-size="limit" layout="prev, pager, next, jumper"
+      :total="total" @current-change="getSkuList" />
+
+
+  </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import skuApi, { type skuInfoDate } from '@/api/sku'
 
+const page = ref(1)
+const limit = ref(10)
+const total = ref(0)
+const skuList = ref<skuInfoDate[]>([])
+const isShowSkuInfo = ref(false)
+const skuInfo = ref<skuInfoDate>({
+  price: 0,
+  skuName: '',
+  skuDesc: '',
+  weight: '',
+  skuDefaultImg: '',
+  skuImageList: [],
+  skuAttrValueList: [],
+  skuSaleAttrValueList: []
+})
+onMounted(()=>{
+  getSkuList()
+})
+
+const getSkuList = async(pager = 1) => {
+  page.value = pager
+  const result = await skuApi.reqSkuList(page.value,limit.value)
+  console.log(result);
+  skuList.value = result.records
+  total.value = result.total
+}
 </script>
 
 <style scoped>

a919ee5cdbc16ea5a9766ed3bea8b6206ae7cf48
liuyuanbo <1131107655@qq.com>
2023-07-30 16:00:03 +0800

    TodaySale-静态搭建

---
+++ src/views/home/components/TopView/components/TodaySale/index.vue
@@ -0,0 +1,79 @@
+<template>
+    <el-card>
+      <div class="title">今日销售额</div>
+      <div class="count">30346788</div>
+      <div class="main">
+        <div class="content">
+          <span class="mr-5">日同比</span>
+          <span class="mr-5">-19.16</span>
+          <span class="icon down"></span>
+        </div>
+        <div class="content">
+          <span class="mr-5">月同比</span>
+          <span class="mr-5">56.66</span>
+          <span class="icon up"></span>
+        </div>
+      </div>
+      <div class="line"></div>
+      <div class="bottom">
+        <span class="mr-10">昨日销售额</span>
+        <span>36000000</span>
+      </div>
+    </el-card>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
+  
+  <style lang="scss" scoped>
+  .title {
+    font-size: 12px;
+    color: #aaa;
+    margin-bottom: 5px;
+  }
+  .count {
+    font-size: 26px;
+    letter-spacing: 1px;
+  }
+  .main {
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    .content {
+      color: #999;
+      font-size: 12px;
+      font-weight: bold;
+      display: flex;
+      align-items: center;
+    }
+  }
+  .line {
+    width: 100%;
+    height: 2px;
+    background-color: #999;
+    margin: 10px 0;
+  }
+  .bottom {
+    font-size: 12px;
+    color: #aaa;
+  }
+  // 箭头
+  .down {
+    margin-top: 5px;
+    display: inline-block;
+    border: 5px solid blue;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+  }
+  .up {
+    margin-top: -5px;
+    display: inline-block;
+    border: 5px solid red;
+    border-left-color: transparent;
+    border-right-color: transparent;
+    border-top-color: transparent;
+  }
+  </style>
--- src/views/home/components/TopView/index.vue
+++ src/views/home/components/TopView/index.vue
@@ -1,22 +1,22 @@
 <template>
     <el-row :gutter="10">
       <el-col :span="6">
-        今日销售额
+        <TodaySale></TodaySale>
       </el-col>
       <el-col :span="6">
-        111
+        今日订单量
       </el-col>
       <el-col :span="6">
-        111
+        交易用户数
       </el-col>
       <el-col :span="6">
-        111
+        累计用户数
       </el-col>
     </el-row>
   </template>
   
   <script setup lang="ts">
-  
+  import TodaySale from './components/TodaySale/index.vue'
   </script>
   
   <style scoped>
